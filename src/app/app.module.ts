dfa07a30976876b25ae306ac5b7a45b2c1f65526
John Lucey <contact@johnlucey.co.za>
2017-09-14 09:08:02 +0200

    completed https://angular.io/tutorial/toh-pt6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,27 @@ import { HeroService } from './hero.service';
 import { RouterModule }   from '@angular/router';
 import { DashboardComponent }   from './dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+import { HeroSearchComponent } from './hero-search.component';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [ 
   	BrowserModule,
   	FormsModule,
-  	AppRoutingModule
+  	AppRoutingModule,
+  	HttpModule,
+  	InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [ 
   	AppComponent, 
   	HeroDetailComponent,
   	HeroesComponent,
-  	DashboardComponent
+  	DashboardComponent,
+  	HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ],
