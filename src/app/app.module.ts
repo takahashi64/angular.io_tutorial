f1d0c65fb5fd26bd31f094648941fab9d3071ed2
John Lucey <contact@johnlucey.co.za>
2017-09-14 08:25:46 +0200

    completed https://angular.io/tutorial/toh-pt5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { RouterModule }   from '@angular/router';
+import { DashboardComponent }   from './dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [ 
   	BrowserModule,
-  	FormsModule
+  	FormsModule,
+  	AppRoutingModule
   ],
   declarations: [ 
   	AppComponent, 
   	HeroDetailComponent,
-  	HeroesComponent
+  	HeroesComponent,
+  	DashboardComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ],
